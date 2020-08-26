c0072fdcfc6b417213fcee5afc6242be7165d9a9
Quinn Slack <qslack@qslack.com>
2020-08-26 00:04:46 -0500

    wip

--- doc/_resources/assets/docs.js
+++ doc/_resources/assets/docs.js
@@ -192,3 +192,20 @@ window.sgdocs = (() => {
   }
 
 })()
+
+function copyText(element) {
+  var range, selection;
+
+  if (document.body.createTextRange) {
+    range = document.body.createTextRange();
+    range.moveToElementText(element);
+    range.select();
+  } else if (window.getSelection) {
+    selection = window.getSelection();
+    range = document.createRange();
+    range.selectNodeContents(element);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
+  document.execCommand('copy');
+}
